6d2a1e14eef298a4ac9811dd80e1ad6bb71510c3
adigaabhirama <156497450+adigaabhirama@users.noreply.github.com>
2024-03-11 16:40:24 +0530

    extend
    extened

---
+++ extra_calculator.java
@@ -0,0 +1,16 @@
+//package cal;
+public class extra_calculator{
+     public static int square;
+     public static void setSquare(int square) {
+         extra_calculator.square = square;
+         square = a*a;
+     }
+    public static int cube;
+    int square(int a){
+        return a*a;
+     }
+    int cube(int b){
+        return b*b*b;
+    }
+    
+}
